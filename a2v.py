e985cfcc1fd52f85c16e4506385e88b779b38b6f
modasserbillah <imtishad@gmail.com>
2018-10-26 17:39:45 +0600

    multi image slideshow with crossfade

--- a2v.py
+++ a2v.py
@@ -1,71 +1,44 @@
 import os
 import sys
-import subprocess
-import json
+import platform
 
-audio_name = sys.argv[1]
-image_names = sys.argv[2:]
-number_of_images = len(image_names) + 1 # +1 because we use the first / cover image twice at both ends
-output_name = audio_name.replace('mp3', 'mp4')
+def scale_and_rename(first_image):
 
-# for target video resolution, input images should be >= the following
-height = 1280
-width = 720
+        count = 1
+        height = 1280
+        width = 720
+        for filename in os.listdir(os.getcwd()):
+                if '.jpg' in filename:
+                        if first_image == filename:
+                                cmd = f"ffmpeg -i {filename} -vf scale={height}:{width} img_0.jpg"
+                        else:
+                                cmd = f"ffmpeg -i {filename} -vf scale={height}:{width} img_{count}.jpg"
+                        os.system(cmd)
+                        count += 1
 
-# get audio length and divide time equally for each image
-args=("ffprobe", "-print_format", "json", "-show_entries", "format=duration","-i", audio_name)
-ffprobeOutput = subprocess.check_output(args).decode('utf-8')
-ffprobeOutput = json.loads(ffprobeOutput)
-duration = ffprobeOutput['format']['duration']
-duration = int(float(duration))
-time_for_each_image = duration // number_of_images
-fade_out_at = time_for_each_image - 1
+def create_video(audio):
+        cmd = (
+                f"ffmpeg -loop 1 -i img_%d.jpg -i {audio} "
+                f"-vf zoompan=d=(4+1)/1:fps=1,framerate=25:interp_start=0:interp_end=255:scene=100 "
+                f"-c:v libx264 -c:a aac -shortest {audio.replace('mp3', 'mp4')}"
+        )
+        os.system(cmd)
 
-# this forces all input images to resize to given height and width above
-#the final string will look like the following, we'll construct it incrementally to avoid repeating
-# filter_string = (
-#     f"\"[0]scale={height}:{width}:force_original_aspect_ratio=decrease,pad={height}:{width}:(ow-iw)/2:(oh-ih)/2,setsar=1[i0];"
-#     f"[1]scale={height}:{width}:force_original_aspect_ratio=decrease,pad={height}:{width}:(ow-iw)/2:(oh-ih)/2,setsar=1[i1];"
-#     f"[2]scale={height}:{width}:force_original_aspect_ratio=decrease,pad={height}:{width}:(ow-iw)/2:(oh-ih)/2,setsar=1[i2];"
-#     f"[3]scale={height}:{width}:force_original_aspect_ratio=decrease,pad={height}:{width}:(ow-iw)/2:(oh-ih)/2,setsar=1[i3];"
-#     f"[i0][i1][i2][i3]concat=n=4\""
-# )
+def clean_up():
+        '''delete scaled and renamed images'''
+        if platform.system == 'Windows':
+                command = 'del '
+        else:
+                command = 'rm '
+        cmd = f"{command} img*"
+        os.system(cmd)
+        print("deleted all scaled images.")
 
-filter_string = f"\""
-for n in range(number_of_images):
-    filter_string += f"[{n}]scale={height}:{width}:force_original_aspect_ratio=decrease,pad={height}:{width}:(ow-iw)/2:(oh-ih)/2,setsar=1, fade=t=in:st=0:d=2,fade=t=out:st={fade_out_at}:d=2[i{n}];"
-
-for n in range(number_of_images):
-    filter_string += f"[i{n}]"
-
-filter_string+= f"concat=n={number_of_images}\""
-
-# for single image video
-# cmd = "ffmpeg -loop 1 -i " + image_name +  " -i " + audio_name + " -shortest -acodec copy " + output_name
-
-# for multi image slideshow video
-# again, the string will look like the following but we use concatenation to avoid repetition 
-# cmd = (
-#     f"ffmpeg -y "
-#     f"-loop 1 -t {time_for_each_image} -i {cover_image} "
-#     f"-loop 1 -t {time_for_each_image} -i {second_image} "
-#     f"-loop 1 -t {time_for_each_image} -i {third_image} "
-#     f"-loop 1 -i {cover_image} " 
-#     f"-i {audio_name} "
-#     f"-filter_complex {filter_string} "
-#     f"-shortest "
-#     f"-c:v libx264 -pix_fmt yuv420p -c:a aac {output_name}"
-# )
-cmd_string = f"ffmpeg -y "
-for image in image_names:
-    cmd_string += f"-loop 1 -t {time_for_each_image} -i {image} "
-
-cmd_string += f"-loop 1 -i {image_names[0]} "
-cmd_string += (
-    f"-i {audio_name} "
-    f"-filter_complex {filter_string} "
-    f"-shortest "
-    f"-c:v libx264 -pix_fmt yuv420p -c:a aac {output_name}"
-)
-
-os.system(cmd_string)
+if __name__ == "__main__":        
+            
+        audio = sys.argv[1] 
+        first_image = sys.argv[2]  
+        scale_and_rename(first_image)      
+        create_video(audio)
+        clean_up()
+        print("conversion complete! check folder.")
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-## Convert mp3 to mp4 using any number of images
+## Convert mp3 to mp4 using any number of images with slide show and crossfade effect
 
 ### Requirements
 **ffmpeg** 
@@ -7,9 +7,9 @@
 
 ## Run instructions
 - place the audio and image files in the same folder as script
-- `python a2v.py <audio file name> <image_file_names separated by space>`
+- `python a2v.py <audio file name> <first image name>`
 - may take some time depending on machine performance.
-- video image size is set to 1280x720, change inside script if necessary. All images used should be **greater than or equal**  this resolution.
+- video image size is set to 1280x720. All images used should be **greater than or equal**  to this resolution.
 
 ## To do
 - add youtube upload script.
